d04f3872d9b550668d261c9f6d196c3a8e829485
Brendow <brendow_sabe@hotmail.com>
2026-07-17 13:02:24 -0300

    Iniciando de fato a analise de dados com SQL

--- 0_criar_banco.sql
+++ 0_criar_banco.sql
@@ -3,3 +3,6 @@
 #  Fase 0 - Banco e tabelas (0_criar_banco.sql): criar o database e as 8 tabelas. As 4 tabelas Raw têm todas 
 # as colunas VARCHAR e sem constraints; as 4 tabelas Silver são tipadas e têm PRIMARY KEY, FOREIGN KEY 
 #e mais 2 constraints por tabela (NOT NULL, CHECK e UNIQUE), declaradas dentro do CREATE TABLE.
+
+
+CREATE DATABASE viagens_2025;
